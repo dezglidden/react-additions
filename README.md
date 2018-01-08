050708ed2c3bfab99b0f7d077c838dd0e9f48f91
Dez Glidden <dez@gemstonesoftware.uk>
2018-01-08 02:33:41 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Additional Packages for Create React App
+
+Install the following additional packages via yarn;
+
+* Axios
+* React Router
+* React Router Dom
---
+++ bin/install-additions.js
@@ -0,0 +1,6 @@
+#! /usr/bin/env node
+var shell = require("shelljs");
+
+shell.exec("yarn add axios --save");
+shell.exec("yarn add react-router --save");
+shell.exec("yarn add react-router-dom --save");
